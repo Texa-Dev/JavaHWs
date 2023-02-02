65f0060c2b6ae6b671c03b0f6cd4ef3af4992734
Texa-Dev <artemtroc26@gmail.com>
2023-02-02 19:55:12 +0200

    refactor

--- src/pack/group/Battlefield.java
+++ src/pack/group/Battlefield.java
@@ -3,16 +3,22 @@ package pack.group;
 import pack.structure.base.Hero;
 
 public class Battlefield {
-    private int size;
+    //private final int size; --> Конветрировал в локальную переменную
     private final Squad s1;
     private final Squad s2;
     private int step;
 
     public Battlefield(int size) {
-        this.size = size;
         s1=new Squad(size);
         s2=new Squad(size);
         step=1;
+    }
+    private void waitStep(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void fight(){
@@ -25,7 +31,7 @@ public class Battlefield {
             System.out.println("Step: " +step++);
             h1=s1.hero();
             h2=s2.hero();
-            System.out.print("Hero "+h1.getName()+" deals damage "+h2.getName()+" ");
+            System.out.print(h1.getName()+" deals damage "+h2.getName()+" ");
             h2.takeDamage(h1.attack());
             System.out.println();
             System.out.printf("%s\t<--\t%s\n", h2,h1);
@@ -33,22 +39,14 @@ public class Battlefield {
             if (!s2.anyAlive()) break;  //Цикл прерываеться если во втором отряде после хода не осталось живых героев
             if(!h2.isAlive()) h2=s2.hero(); // Проверка живой ли герой после хода, иначе походит атака от героя с 0hp
 
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+         waitStep();
 
-            System.out.print("Hero "+h2.getName()+" deals damage "+h1.getName()+" ");
+            System.out.print(h2.getName()+" deals damage "+h1.getName()+" ");
             h1.takeDamage(h2.attack());
             System.out.println();
             System.out.printf("%s\t-->\t%s\n", h2,h1);
 
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+          waitStep();
         }
         System.out.println();
         System.out.println(s1.anyAlive()?"Squad 1 WIN":"Squad 2 WIN");
